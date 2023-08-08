2f600e725b31fd2557a9876a0949bab78c17d6e7
franchesco famous <franchescofamous@gmail.com>
2023-08-08 10:34:21 +0000

    end of variable

---
+++ partie1/index.js
@@ -0,0 +1,18 @@
+let nom; // declaration de variable
+nom = "Valentin"; // affectation de variable
+//camelCase
+let nomEtudiant = "Reddington";
+
+const nombreDevie = 100;
+console.log(nom, nomEtudiant, nombreDevie);
+
+console.log(5);
+
+//let age = prompt("quelle est votre age?");
+
+//console.log(age);
+
+let nomEwe = prompt("Quelle est votre nom ewe");
+let prenom = prompt("Quelle est votre prenom");
+
+console.log("bienvenue " + prenom);
